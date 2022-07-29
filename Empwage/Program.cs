0b5ae8e1729972061dbfa808202bead148b04ec9
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-07-29 21:16:33 +0530

    [jayesh] Emp present or not

---
+++ Empwage/Program.cs
@@ -0,0 +1,22 @@
+﻿using Empwage;
+
+namespace EmployeeWageComputation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for a particular problem:");
+            Console.WriteLine("1 for checking Employee present or absent");
+
+            int Check = Convert.ToInt32(Console.ReadLine());
+            switch (Check)
+            {
+                case 1:
+                    EmpPresentOrNot empPresentAbsent = new EmpPresentOrNot();
+                    empPresentAbsent.CheckEmpPresentAbsent();
+                    break;
+            }
+        }
+    }
+ }
